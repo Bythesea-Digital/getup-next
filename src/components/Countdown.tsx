ecc6223517303677560ba9f2995d2a9b2a6b1bd9
Felipe <jflobom@gmail.com>
2021-02-23 22:28:11 -0500

    Add countdown functionality

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,16 +1,54 @@
 import styles from '../styles/components/Countdown.module.css';
+import { useEffect, useState } from 'react';
+
 export function Countdown() {
+  const POMODORO_TIME_SECONDS = 25 * 60;
+  const [time, setTime] = useState(POMODORO_TIME_SECONDS);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes)
+    .padStart(2, '0')
+    .split('');
+  const [secondLeft, secondRight] = String(seconds)
+    .padStart(2, '0')
+    .split('');
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
+  function startCountdown() {
+    setActive(!active);
+  }
+
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      <button
+        onClick={startCountdown}
+        type="button"
+        className={styles.countdownButton}
+      >
+        {active ? 'Pausar el ciclo' : 'Iniciar un ciclo'}
+      </button>
     </div>
   );
 }
